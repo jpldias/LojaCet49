90e1af4bc0780196969f482b039fbf1de7ef2278
Joao Dias <Joao.Lourenco.Dias@formandos.cinel.pt>
2020-12-14 22:45:09 +0000

    criar a base de dados e tabela produtos

---
+++ LojaCet49/Dados/Entidades/Produto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LojaCet49.Dados.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Preco { get; set; }
+
+
+        [Display(Name = "Image")]
+        public string UrlDaImagem { get; set; }
+
+
+        [Display(Name = "Ultima Compra")]
+        public DateTime UltimaCompra { get; set; }
+
+
+        [Display(Name = "Ultima Venda")]
+        public DateTime UltimaVenda { get; set; }
+
+
+        [Display(Name = "Disponibilidade")]
+        public bool Disponivel { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+
+
+    }
+}
